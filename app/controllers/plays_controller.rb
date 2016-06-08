850e9898526ebfdccf9bb612b19c0ad204e609e4
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-06-08 20:55:10 +0000

    Added New and Create Actions with view Files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -3,4 +3,24 @@ class PlaysController < ApplicationController
   def index
     
   end
+  
+  def new
+    @play = Play.new
+  end
+  
+  def create
+    @play = Play.new(play_params)
+  end
+  
+  def destroy
+  
+  end
+  
+  
+  private
+  
+  def play_params
+    params.require(:play).permit(:title, :description, :director)
+  end
+  
 end
