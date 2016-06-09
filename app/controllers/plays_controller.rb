2e330a0a5a1b877d1b63ff10ddfe87628d0f2ae1
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-06-09 01:38:39 +0000

    Added Plays to App Index page

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,7 +1,13 @@
 class PlaysController < ApplicationController
   
+  before_action :find_play, only: [:show, :edit, :update, :destroy]
+  
   def index
-    
+    @plays = Play.all.order("created_at DESC")
+  end
+  
+  def show
+    @play = Play.find(params[:id])
   end
   
   def new
@@ -10,6 +16,12 @@ class PlaysController < ApplicationController
   
   def create
     @play = Play.new(play_params)
+    
+    if @play.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
   
   def destroy
@@ -23,4 +35,7 @@ class PlaysController < ApplicationController
     params.require(:play).permit(:title, :description, :director)
   end
   
+  def find_play
+    @play = Play.find(params[:id])
+  end
 end
